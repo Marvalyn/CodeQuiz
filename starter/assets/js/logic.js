29b044c768db6177eb2eb7c029bbf9a3100c71d9
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-01-13 18:38:48 +0000

    addition of starter files

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,30 @@
+// ## User Story
+
+// ```
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+// ```
+
+// ## Acceptance Criteria
+
+// Create a code quiz that contains the following requirements:
+
+// * A start button that when clicked a timer starts and the first question appears.
+//reference colour splosion timers & intervals
+ 
+//   * Questions contain buttons for each answer.
+//   * 
+//   * When answer is clicked, the next question appears
+//will need an event listener button "click"
+//   * 
+//   * If the answer clicked was incorrect then subtract time from the clock
+// will need an if loop
+// * The quiz should end when all questions are answered or the timer reaches 0.
+
+//   * When the game ends, it should display their score and give the user the ability to save their initials and their score
+//client side storage will need get event and set event
+//will need a prevent default declaration
+
+// does it matter how many questions?
+  
