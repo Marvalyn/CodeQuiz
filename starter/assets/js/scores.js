ca66bb6069ff8e202cbe034045e97339117e2eed
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-01-15 20:17:58 +0000

    the highscores board

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,9 +1,9 @@
 // get values from local storage
 // window.localStorage.getItem("UserInitials");
 //ccreate and variable retrive objects from local storage
-var highScore = localStorage.getItem("UserInitials");
+var highScore = window.localStorage.getItem("UserInitials");
 console.log(highScore);
-var showScore = JSON.parse(localStorage.getItem("UserInitials"));
+var showScore = JSON.parse(highScore);
 console.log(showScore);
 
 UserID = document.getElementById("highscores");
@@ -13,15 +13,13 @@ for (var i = 0; i < showScore.length; i++) {
     var li = document.createElement("li");
     UserID.appendChild(li);
     li.textContent = showScore[i];
-}
+};
 
 
-//create li tags 
 //set text content to values from object user initials and score
 //user
 //append li tags to ol element
-//select ol element from html
-//sort values from highscore to low score
+
 //clear data from local storage 
 var clearButton = document.getElementById("clear")
 
